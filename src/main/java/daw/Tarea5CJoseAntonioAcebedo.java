b5b3ec343f779795e75e1bb99e1ac21cdaeaa423
acebedo <jaceara2006@g.educaand.es>
2024-01-13 11:06:37 +0100

    Inicio del Proyecto

---
+++ src/main/java/daw/Tarea5CJoseAntonioAcebedo.java
@@ -0,0 +1,12 @@
+package daw;
+
+/**
+ *
+ * @author acebedo
+ */
+public class Tarea5CJoseAntonioAcebedo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
